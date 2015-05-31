7ace41e762224be57926c4ea8b551c9b3b51ee61
karaAJC <KaraAJC@gmail.com>
2015-05-31 17:12:16 -0500

    updating models

--- db/migrate/20150412133249_create_favorites.rb
+++ db/migrate/20150412133249_create_favorites.rb
@@ -1,7 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
-
+      t.references :bowl
+      t.references :user
       t.timestamps
     end
   end
--- db/migrate/20150412133319_create_bowls.rb
+++ db/migrate/20150412133319_create_bowls.rb
@@ -1,7 +1,7 @@
 class CreateBowls < ActiveRecord::Migration
   def change
     create_table :bowls do |t|
-
+      t.string :name
       t.timestamps
     end
   end
